d43252d2772654f2d1327c93a71544051431e045
DW-Roshan <roshan@dreamweaversindia.com>
2025-04-24 23:43:58 +0530

    Add User form complete

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,10 @@ use App\Models\Branch;
 use App\Models\Designation;
 use App\Models\Division;
 use App\Models\Station;
+use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
@@ -29,15 +32,11 @@ class UserController extends Controller
     public function store(Request $request)
     {
 
-        // return response()->json([
-        //     'message' => 'User added successfully',
-        //     'data' => $request->all(),
-        // ]);
         $validator = Validator::make($request->all(),[
             'firstName' => 'required|string',
             'lastName' => 'required|string',
             'email' => 'required|email|unique:users,email',
-            'username' => 'required|string|unique:users,username',
+            'username' => 'required|alpha_num:ascii|unique:users,username',
             'password' => 'required|confirmed|string|min:6',
             'branch' => 'required|exists:branches,id',
             'division' => 'required|exists:divisions,id',
@@ -51,35 +50,74 @@ class UserController extends Controller
             'gPay' => 'required|string'
         ]);
 
-        // $validated = $request->validate([
-        //     'firstName' => 'required|string',
-        //     'lastName' => 'required|string',
-        //     'email' => 'required|email|unique:users,email',
-        //     'username' => 'required|string|unique:users,username',
-        //     'password' => 'required|confirmed|string|min:6',
-        //     'branch' => 'required|exists:branches,id',
-        //     'division' => 'required|exists:divisions,id',
-        //     'designation' => 'required|exists:designations,id',
-        //     'station' => 'required|exists:stations,id',
-        //     'dateOfBirth' => 'required|date',
-        //     'dateOfJoining' => 'required|date',
-        //     'loginValidUpto' => 'required|date',
-        //     'phoneNumber' => 'required|string',
-        //     'payBand' => 'required|string',
-        //     'gPay' => 'required|string'
-        // ]);
-
         if($validator->fails()){
             return response()->json([
                 'message' => 'Validation failed',
                 'errors' => $validator->errors()
             ], 422);
         }
+
         $validated = $validator->validated();
 
-        return response()->json([
-            'message' => 'User added successfully',
-            'data' => $validated,
-        ]);
+        if($validated) {
+
+            try {
+
+                $user = User::create([
+                    'user_type' => 'U',
+                    'username' => $request->username,
+                    'email' => $request->email,
+                    'password' => Hash::make($request->password),
+                    'first_name' => $request->firstName,
+                    'last_name' => $request->lastName,
+                    'date_of_birth' => Carbon::parse($request->dateOfBirth)->toDateString(),
+                    'date_of_joining' => Carbon::parse($request->dateOfJoining)->toDateString(),
+                    'gender' => $request->gender,
+                    'pf_no' => $request->pfNo,
+                    'father_name' => $request->fatherName,
+                    'first_class_duty_pass' => $request->firstClassDutyPassNo,
+                    'branch_id' => $request->branch,
+                    'division_id' => $request->division,
+                    'designation_id' => $request->designation,
+                    'station_head_quarter_id' => $request->station,
+                    'mobile_no' => $request->phoneNumber,
+                    'authority_no' => $request->checkingAuthority,
+                    'expiry_date' => Carbon::parse($request->loginValidUpto)->toDateString(),
+                    'ta_sr_no' => $request->taSrNo,
+                    'incentive_amount' => $request->incentiveAmt,
+                    'incentive_percentage' => $request->incentivePercentage,
+                    'pay_band' => $request->payBand,
+                    'g_pay' => $request->gPay
+                ]);
+
+                if($user){
+
+                    return response()->json([
+                        'message' => 'User added successfully.',
+                    ]);
+
+                }else{
+
+                    return response()->json([
+                        'message' => 'User not added, something went wrong.'
+                    ], 500);
+
+                }
+            } catch (\Throwable $th) {
+
+                return response()->json([
+                    'message' => 'User not added, something went wrong.',
+                    'throws' => $th
+                ], 500);
+            }
+
+        } else {
+
+            return response()->json([
+                'message' => 'User not added, something went wrong.'
+            ], 500);
+
+        }
+
     }
 }
