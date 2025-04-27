734683d4217f9187ed078a564f7d70131ea970d1
DW-Roshan <roshan@dreamweaversindia.com>
2025-04-27 17:42:31 +0530

    Add user_type in login response

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -23,7 +23,7 @@ class AuthController extends Controller
 
         $token = $user->createToken('tteportal')->accessToken;
 
-        return response()->json(['token' => $token, 'name' => $user->first_name." ".$user->last_name, 'email' => $user->email]);
+        return response()->json(['token' => $token, 'user_type' => $user->user_type, 'name' => $user->first_name." ".$user->last_name, 'email' => $user->email]);
     }
 
     public function logout(Request $request)
